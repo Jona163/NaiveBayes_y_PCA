76ec69f06578977a9aeb2068d36708b92eaee6b0
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-20 20:29:42 -0600

    Update naive_bayes.py

--- naive_bayes/naive_bayes.py
+++ naive_bayes/naive_bayes.py
@@ -2,3 +2,23 @@
 # Fecha: 20 Septiembre 2024
 # Descripción: naive_bayes.
 # GitHub: https://github.com/Jona163
+
+import numpy as np
+
+class NaiveBayes:
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self._classes = np.unique(y)
+        n_classes = len(self._classes)
+
+        # calculate mean, var, and prior for each class
+        self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._priors = np.zeros(n_classes, dtype=np.float64)
+
+        for idx, c in enumerate(self._classes):
+            X_c = X[y == c]
+            self._mean[idx, :] = X_c.mean(axis=0)
+            self._var[idx, :] = X_c.var(axis=0)
+            self._priors[idx] = X_c.shape[0] / float(n_samples)
