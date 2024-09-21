e01b6657d256197cbc678925fde488551ead3cdd
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-20 20:34:05 -0600

    Update pca.py

--- PCA/pca.py
+++ PCA/pca.py
@@ -2,3 +2,27 @@
 # Fecha: 20 Septiembre 2024
 # Descripción: PCA.
 # GitHub: https://github.com/Jona163
+
+import numpy as np
+
+
+class PCA:
+
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.components = None
+        self.mean = None
+
+    def fit(self, X):
+        # mean centering
+        self.mean = np.mean(X, axis=0)
+        X = X -  self.mean
+
+        # covariance, functions needs samples as columns
+        cov = np.cov(X.T)
+
+        # eigenvectors, eigenvalues
+        eigenvectors, eigenvalues = np.linalg.eig(cov)
+
+        # eigenvectors v = [:, i] column vector, transpose this for easier calculations
+        eigenvectors = eigenvectors.T
